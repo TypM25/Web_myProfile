c314c499c1b2c357cf1463472eb407f94933f49b
Jirapat Ruetrakul <125210731+JRsssssss@users.noreply.github.com>
2024-07-27 22:48:23 +0700

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className='flex justify-center h-14 w-auto bg-stone-700'>
         <div className='text-center my-auto'>
-            <p className='text-describeFont text-white '>@Typm company</p>
+            <p className='text-describeFont text-white '>@Thanyaporn Musikpodok</p>
         </div>
     </div>
   )
